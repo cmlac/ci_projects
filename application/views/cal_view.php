efbf2b14549864bdda89341910c3556bdf7540de
DanielF <1704249@student.swin.edu.au>
2012-10-18 12:01:04 +1100

    a few small edits
    Added a link to the calendar in the menu

--- application/views/cal_view.php
+++ application/views/cal_view.php
@@ -3,7 +3,7 @@
 	<head>
 		<title>CMLAC Calendar</title>
 		<meta charset="UTF-8">
-		<link rel="stylesheet" type"text/css" href="<?php echo base_url()?>css/cmlac_style.css " />
+		<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>css/cmlac_style.css " />
 		<script type="text/javascript" src=" https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 	</head>
 	<body>
